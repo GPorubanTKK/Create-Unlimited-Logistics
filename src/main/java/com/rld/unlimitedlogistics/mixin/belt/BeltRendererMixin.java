a88afa06553760cabf7ff256a52d21e8a56321d9
RandomLonelyDev <gedeon@porubannursery.com>
2025-08-25 13:21:25 -0400

    Items move correctly (inv and visibly) however opposite to belt direction

--- src/main/java/com/rld/unlimitedlogistics/mixin/belt/BeltRendererMixin.java
+++ src/main/java/com/rld/unlimitedlogistics/mixin/belt/BeltRendererMixin.java
@@ -21,13 +21,17 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.logging.Logger;
+
+import static com.rld.unlimitedlogistics.CreateUnlimitedLogistics.LOGGER;
+
 //handles itemstack rendering/movement
 @Mixin(BeltRenderer.class)
 public class BeltRendererMixin {
     @ModifyVariable(method = "renderItems", at = @At(value = "STORE"), name = "verticality")
     private int modifyVerticality(int value, @Local(argsOnly = true) BeltBlockEntity be) {
         BlockState state = be.getBlockState();
-        return switch(state.getValue(BeltBlock.SLOPE)) { //TODO: Find correct rotation for item
+        return switch(state.getValue(BeltBlock.SLOPE)) {
             case UPWARD -> 1;
             case DOWNWARD -> -1;
             default -> 0;
@@ -41,11 +45,25 @@ public class BeltRendererMixin {
         @Local(argsOnly = true) BeltSlope slope
     ) {
         if((!onSlope) || slope == BeltSlope.VERTICAL) return 0;
-        //if(slope == BeltSlope.VERTICAL) return 0;
         return tiltForward ? -45 : 45;
     }
 
-    @Inject(method = "renderItem", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;pushPose()V"))
+    @WrapOperation(method = "renderItem", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(DDD)V", ordinal = 0))
+    private void wrapTranslate(
+        PoseStack instance,
+        double x, double y, double z,
+        Operation<Void> original, @Local(argsOnly = true) Direction beltFacing, @Local(argsOnly = true) BeltSlope slope
+    ) {
+        if(slope == BeltSlope.VERTICAL) {
+            original.call(instance, 0d, switch(beltFacing.getAxis()) {
+                case X -> x;
+                case Y -> y;
+                case Z -> z;
+            }, 0d);
+        } else original.call(instance, x, y, z);
+    }
+
+    @Inject(method = "renderItem", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;pushPose()V", ordinal = 0))
     private void printItemLocation(
         BeltBlockEntity be, float partialTicks, PoseStack ms,
         MultiBufferSource buffer, int light, int overlay,
@@ -54,30 +72,5 @@ public class BeltRendererMixin {
         TransportedItemStack transported, Vec3 beltStartOffset, CallbackInfo ci,
         @Local(name = "itemPos") Vec3 itemPos
     ) {
-        Minecraft.getInstance().player.sendSystemMessage(Component.literal(String.format("%fx %fy %fz", itemPos.x, itemPos.y, itemPos.z)));
-        //find where the items are moving over each line
-    }
-
-    @WrapOperation(method = "renderItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos$MutableBlockPos;move(III)Lnet/minecraft/core/BlockPos$MutableBlockPos;"))
-    private BlockPos.MutableBlockPos wrapMove(BlockPos.MutableBlockPos instance, int x, int y, int z, Operation<BlockPos.MutableBlockPos> original) {
-        return original.call(instance, 0, y, 0); //only apply vertical movement
-    }
-
-    @Inject(
-        method = "renderItem",
-        at = @At(
-            value = "INVOKE",
-            target = "Lcom/mojang/blaze3d/vertex/PoseStack;mulPose(Lorg/joml/Quaternionf;)V",
-            shift = At.Shift.AFTER
-        )
-    )
-    private void onMulPose(
-        BeltBlockEntity be, float partialTicks, PoseStack ms,
-        MultiBufferSource buffer, int light, int overlay,
-        Direction beltFacing, Vec3i directionVec, BeltSlope slope,
-        int verticality, boolean slopeAlongX, boolean onContraption,
-        TransportedItemStack transported, Vec3 beltStartOffset, CallbackInfo ci
-    ) {
-        ms.mulPose(Axis.YP.rotationDegrees(180)); //try to ensure items are rendered upright on the belt, regardless of isItemUpright()
     }
 }
